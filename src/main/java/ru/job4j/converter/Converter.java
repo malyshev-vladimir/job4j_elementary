4f6d7e6c5a5519c088e11239aed6f38ea7ded063
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-03-08 16:54:20 +0100

    5. Конвертер валюты [#41583]. Исправление.

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -16,6 +16,6 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
         float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " euro.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
     }
 }
