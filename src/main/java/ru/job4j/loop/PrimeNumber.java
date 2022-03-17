0e1d234032f52a832f58167b3b768572b4937fb5
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-03-17 12:37:58 +0100

    5.6. Простые числа [#156314]

---
+++ src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int counter = 0;
+        for (int number = 1; number <= finish; number++) {
+            if (CheckPrimeNumber.check(number)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
