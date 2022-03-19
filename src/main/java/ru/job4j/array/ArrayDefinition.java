05c70ca1f8db487fa6a89276c2a10b9bfffa8393
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-03-19 12:58:09 +0100

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Ivan Petrov";
+        names[2] = "Vlad Ionov";
+        names[3] = "Viktor Atrukin";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
