ea6118a9cc5bebca4642f9f6fa507dc050714876
malyshev-vladimir <malyshev.vladimir.a@gmail.com>
2022-03-23 15:17:48 +0100

    6.5.3. Сортировка выборкой. [#156320]. Дополнение тестов.

--- src/test/java/ru/job4j/array/SortSelectedTest.java
+++ src/test/java/ru/job4j/array/SortSelectedTest.java
@@ -4,11 +4,27 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class SortSelectedTest {
+
     @Test
     public void whenSortArrayOf5elements() {
         int[] data = new int[] {3, 4, 1, 2, 5};
         int[] result = SortSelected.sort(data);
         int[] expected = new int[] {1, 2, 3, 4, 5};
+        Assert.assertArrayEquals(expected, result);
+    }
+    @Test
+    public void whenSortArrayOf3elements() {
+        int[] data = new int[] {64, 12, 23};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {12, 23, 64};
+        Assert.assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void whenSortArrayOf5elements2() {
+        int[] data = new int[] {8, 2, 3, 5, 17};
+        int[] result = SortSelected.sort(data);
+        int[] expected = new int[] {2, 3, 5, 8, 17};
         Assert.assertArrayEquals(expected, result);
     }
 }
